52afa08fbddd0fad1b8bf98aeb841f62139b5eb7
Taha Gargouri <taha.gargouri@itcexpertise.com>
2023-05-28 22:48:43 +0200

    initial commit

---
+++ screens/Chat.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import {GiftedChat} from "react-native-gifted-chat" 
+
+export default function  () {
+  return (
+
+    <GiftedChat/> 
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/Login.js
@@ -0,0 +1,6 @@
+import React ,{useState} from 'react' ;
+import { StyleSheet, Text , Button , Text ,  } from 'react-native/types';
+import {signInWithEmailndPassword} from 'firebase/auth' ;
+import {auth} from "../config/firebase" ;
+
+const backImage = require("../assets/backImage.png") ;
